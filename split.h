049f51682c36b6c922e743e2bd54b00a9b91f054
Miles <milesdanielcross@gmail.com>
2025-03-21 16:29:07 -0400

    Update split.h

--- split.h
+++ split.h
@@ -5,12 +5,12 @@
 std::vector<std::string> split(std::string str, std::string delimiter)
 {
     char* cstr = const_cast<char*>(str.c_str());
-    char* current=strtok(cstr,delimiter.c_str());
+    char* current = strtok(cstr, delimiter.c_str());
     std::vector<std::string> arr;
     
-    while(current!=NULL){
+    while(current != NULL){
         arr.push_back(current);
-        current=strtok(NULL, delimiter.c_str());
+        current = strtok(NULL, delimiter.c_str());
     }
     return arr;
 }
