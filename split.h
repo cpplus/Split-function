c0841ce56eede008d5fb34bcd172b9397290ed22
Miles <milesdanielcross@gmail.com>
2025-03-21 16:27:09 -0400

    Create split.h

---
+++ split.h
@@ -0,0 +1,16 @@
+#include <string>
+#include <string.h>
+#include <vector>
+
+std::vector<std::string> split(std::string str, std::string delimiter)
+{
+    char* cstr = const_cast<char*>(str.c_str());
+    char* current=strtok(cstr,delimiter.c_str());
+    std::vector<std::string> arr;
+    
+    while(current!=NULL){
+        arr.push_back(current);
+        current=strtok(NULL, delimiter.c_str());
+    }
+    return arr;
+}
