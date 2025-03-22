55027375dbc756441a67ef94e1c9881777478e45
Miles <milesdanielcross@gmail.com>
2025-03-22 06:52:06 -0400

    Update round.h

--- round.h
+++ round.h
@@ -1,5 +1,17 @@
 #include <math.h>
+
+
 int roundTo(int num, int nearest)
 {
     return round(double(num) / nearest) * nearest;
+}
+
+int floorTo(int num, int nearest)
+{
+    return floor(double(num) / nearest) * nearest;
+}
+
+int ceilTo(int num, int nearest)
+{
+    return ceil(double(num) / nearest) * nearest;
 }
