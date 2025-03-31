a40d6104a03349ace4efc5baadf4a6617e54b844
Miles <milesdanielcross@gmail.com>
2025-03-31 19:13:32 -0400

    Update random.h

--- random.h
+++ random.h
@@ -1,6 +1,6 @@
 #include <random>
 
-int random(int min, int max) {
+inline int random(int min, int max) {
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> distrib(min, max);
