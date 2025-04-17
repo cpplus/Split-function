880e2d07711341979ab43f89d7cc49f136539888
Miles <milesdanielcross@gmail.com>
2025-04-17 08:55:33 -0400

    Update random.h

--- random.h
+++ random.h
@@ -1,5 +1,6 @@
 #include <random>
 #include <cstring>
+#include <string>
 const int random(const int min, const int max) {
     std::random_device rd;
     std::mt19937 gen(rd());
@@ -7,6 +8,15 @@ const int random(const int min, const int max) {
     return distrib(gen);
 }
 
-const char randChar(const char* charset = AsciiCharset) {
+const char randChar(const char* charset = AsciiCharset()) {
     return charset[ random(0, std::strlen(charset) - 1) ];
+} 
+
+const char* randStr(const int length, const char* charset = AsciiCharset()) {
+    std::string str = "";
+    for (int i = 0; i < length; i++)
+        str += randChar(charset);
+    char* chr = new char[str.size() + 1];
+    std::strcpy(chr,str.c_str());
+    return chr;
 } 
