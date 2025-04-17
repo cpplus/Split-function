75c930b39d04917de469e149f21f2679e8629e89
Miles <milesdanielcross@gmail.com>
2025-04-16 20:31:21 -0400

    Update random.h

--- random.h
+++ random.h
@@ -1,9 +1,12 @@
 #include <random>
-
-const inline int random(const int min, const int max) {
+#include <cstring>
+const int random(const int min, const int max) {
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> distrib(min, max);
-
     return distrib(gen);
 }
+
+const char randChar(const char* charset = AsciiCharset) {
+    return charset[ random(0, std::strlen(charset) - 1) ];
+} 
