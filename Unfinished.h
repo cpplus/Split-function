a0435816b8cdfab92e66157348af759a0ad0502f
Miles <milesdanielcross@gmail.com>
2025-04-19 20:57:36 -0400

    Update Unfinished.h

--- Unfinished.h
+++ Unfinished.h
@@ -10,4 +10,18 @@ CtrTen getCT(std::vector<int> vals) {
     for (int i = 0; i < vals.size(); i++)
         ct.mean += vals[i];
     ct.mean /= vals.size();
+
+    // get mode
+    std::unordered_map<int, int> counts;
+    for (int num : vec) {
+        counts[num]++;
+    }
+
+    int maxCount = 0;
+    for (const auto& pair : counts) {
+        if (pair.second > maxCount) {
+            maxCount = pair.second;
+            ct.mode = pair.first;
+        }
+    }
 } 
