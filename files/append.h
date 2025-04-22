d1e203752f622b5fccf1e420dd79cdca0dfed8f8
Miles <milesdanielcross@gmail.com>
2025-04-21 20:49:59 -0400

    Update append.h

--- files/append.h
+++ files/append.h
@@ -1,12 +1,8 @@
 #include <cstring>
 
-char* appendChar(char* str, char c) {
-    if (str == nullptr) {
-        char* newStr = new char[2];
-        newStr[0] = c;
-        newStr[1] = '\0';
-        return newStr;
-    }
+const char* appendChar(char* str, char c) {
+    if (str == nullptr)
+        return c + "\0";
 
     size_t length = strlen(str);
     char* newStr = new char[length + 2];
@@ -15,4 +11,3 @@ char* appendChar(char* str, char c) {
     newStr[length + 1] = '\0';
     delete[] str;
     return newStr;
-}
