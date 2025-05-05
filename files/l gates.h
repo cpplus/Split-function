537c34c6ec4c964c95938b060516dc4bb09b7e8a
Miles <milesdanielcross@gmail.com>
2025-05-05 06:48:01 -0400

    Update l gates.h

--- files/l gates.h
+++ files/l gates.h
@@ -1,30 +1,30 @@
 // logic gates
 bool NOT(bool a) {
-     return 0 ^ a;
+    return 1 ^ a;
 }
 
 bool AND(bool a, bool b) {
-
+    return a + b > 1;
 }
 
 bool NAND(bool a, bool b) {
-
+    return NOT(a * b);
 }
 
 bool OR(bool a, bool b) {
-
+    return a + b > 0
 }
 
 
 bool NOR(bool a, bool b) {
-
+    return NOT(a + b)
 }
 
 bool XOR(bool a, bool b) {
-
+    return a ^ b;
 }
 
 
 bool NXOR(bool a, bool b) {
-
+    return 1 ^ a ^ b;
 }
